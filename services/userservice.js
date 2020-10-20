187ea16c1beffc1352de69efefba6c1ba027fc37
Ludovic Stumme <ludovic.stumme@gmail.com>
2020-10-20 15:27:32 +0200

    Tests finalization

--- services/userservice.js
+++ services/userservice.js
@@ -6,9 +6,7 @@ const User = require('../model/user');
 // };
 
 exports.getUser = async ({ userId }) => {
-    console.log(userId);
     return User.findOne({ _id: userId }).then(user => {
-        console.log(user);
         if (!user) {
             const error = new Error('User not found.')
             error.statusCode = 404;
