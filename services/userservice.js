fed070e274fe554466786b9a316010597779c141
Ludovic Stumme <ludovic.stumme@gmail.com>
2020-10-24 01:32:36 +0200

    Adding getUsers services function

--- services/userservice.js
+++ services/userservice.js
@@ -2,17 +2,37 @@ const User = require('../model/user');
 
 
 exports.getUsers = async ({ page, perPage }) => {
+    return User.countDocuments()
+        .then(count => {
+            const pageCount = Math.trunc(count / perPage) + (count % perPage > 0 ? 1 : 0);
+            if (count <= perPage * (page - 1) || (perPage * (page - 1) < 0)) {
+                const error = new Error('Pagination out of bounds.');
+                error.statusCode = 400;
+                throw error;
+            }
+            return User.find().skip((page - 1) * perPage).limit(perPage)
+                .select('login firstname lastname email avatar')
+                .then(result => {
+                    return {
+                        users: result,
+                        pageCount: pageCount
+                    };
+
+                })
+        });
 };
 
 exports.getUser = async ({ userId }) => {
-    return User.findOne({ _id: userId }).then(user => {
-        if (!user) {
-            const error = new Error('User not found.')
-            error.statusCode = 404;
-            throw error;
-        }
-        return user;
-    });
+    return User.findOne({ _id: userId })
+        .select('login firstname lastname email avatar')
+        .then(user => {
+            if (!user) {
+                const error = new Error('User not found.')
+                error.statusCode = 404;
+                throw error;
+            }
+            return user;
+        });
 };
 
 
--- test/user-services.js
+++ test/user-services.js
@@ -47,6 +47,7 @@ describe('User Controller', function () {
                 .then(result => {
                     expect(result).to.have.property('_id');
                     expect(result._id.toString()).to.equal(registeredUser._id.toString());
+                    expect(result).to.not.have.own.property('password');
                     done();
                 })
                 .catch(err => {
@@ -55,4 +56,80 @@ describe('User Controller', function () {
                 })
         });
     });
+
+    describe('#getUsers', function () {
+        let registeredUser;
+        before(async () => {
+            await dbHandler.connect();
+        });
+
+        after(async () => {
+            await dbHandler.closeDatabase();
+        });
+
+        beforeEach(async () => {
+            for (let i = 0; i < 20; i++) {
+                const user = new User({
+                    login: 'user' + i,
+                    password: 'password',
+                    email: 'user' + i + '@user.com'
+                });
+                await user.save();
+            }
+        });
+
+        afterEach(async () => {
+            await dbHandler.clearDatabase();
+        });
+
+        it('should throw an error if range out of bounds', function (done) {
+            userservice.getUsers({ page: 3, perPage: 10 })
+                .then(result => {
+                    assert.fail('Error');
+                })
+                .catch(err => {
+                    expect(err).to.have.property('message', 'Pagination out of bounds.');
+                    expect(err).to.have.property('statusCode', 400);
+                    done();
+                })
+        });
+
+        it('should return an object contianing the required data and the number of pages', function (done) {
+            const perPage = 10;
+            userservice.getUsers({ page: 1, perPage: perPage })
+                .then(result => {
+                    expect(result).to.have.property('pageCount', 2);
+                    expect(result).to.have.property('users').to.have.lengthOf(perPage);
+                    for (let i = 0; i < perPage; i++) {
+                        expect(result.users[i]).to.not.have.own.property('password');
+                    }
+                    done();
+                })
+                .catch(err => {
+                    console.log(err);
+                    assert.fail('Error');
+                    done();
+                })
+        });
+
+        it('should return an object contianing the required data and the number of pages 2', function (done) {
+            const perPage = 7;
+            userservice.getUsers({ page: 1, perPage: perPage })
+                .then(result => {
+                    expect(result).to.have.property('pageCount', 3);
+                    expect(result).to.have.property('users').to.have.lengthOf(perPage);
+                    for (let i = 0; i < perPage; i++) {
+                        expect(result.users[i]).to.not.have.own.property('password');
+                    }
+                    done();
+                })
+                .catch(err => {
+                    console.log(err);
+                    assert.fail('Error');
+                    done();
+                })
+        });
+
+    });
+
 });
