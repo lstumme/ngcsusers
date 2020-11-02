ad669b8518a7dc2a8b12e1f65cabc6a266348271
Ludovic Stumme <ludovic.stumme@gmail.com>
2020-11-03 00:53:47 +0100

    updating index

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 module.exports = {
     User: require('./model/user'),
     UserRoutes: require('./routes/userroutes'),
+    UserController: require('./controllers/usercontroller'),
     AuthRoutes: require('./routes/authroutes'),
     isAuth: require('./middleware/is-auth'),
+
 };
 
