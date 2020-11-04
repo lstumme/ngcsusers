eaabde6573ab0676d0ccf9c8f59b6eb5edbf878f
Ludovic Stumme <ludovic.stumme@gmail.com>
2020-11-05 00:51:13 +0100

    Adding UserServices to index

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ module.exports = {
     User: require('./model/user'),
     UserRoutes: require('./routes/userroutes'),
     UserController: require('./controllers/usercontroller'),
+    UserServices: require('./services/userservices'),
     AuthRoutes: require('./routes/authroutes'),
     isAuth: require('./middleware/is-auth'),
 
